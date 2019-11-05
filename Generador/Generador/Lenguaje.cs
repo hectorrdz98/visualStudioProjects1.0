c48e8e4862230fba00345dd97c38a7b396a4628d
Hector Rodriguez <hd.rdz98@gmail.com>
2019-11-05 09:55:54 -0600

    Changes 05-11-2019

--- Generador/Generador/Lenguaje.cs
+++ Generador/Generador/Lenguaje.cs
@@ -25,20 +25,46 @@ namespace Generador
             Match(";");
             Match("{");
             {
-                Producciones();
+                // https://pastebin.com/VxN0hr2v
+                lenguaje.WriteLine("using System;");
+                lenguaje.WriteLine("using System.Collections.Generic;");
+                lenguaje.WriteLine("using System.Linq;");
+                lenguaje.WriteLine("using System.Text;");
+                lenguaje.WriteLine("using System.Threading.Tasks;");
+                lenguaje.WriteLine();
+                lenguaje.WriteLine("namespace Generador");
+                lenguaje.WriteLine("{");
+                {
+                    lenguaje.WriteLine("\tclass Lenguaje : Sintaxis");
+                    lenguaje.WriteLine("\t{");
+                    {
+                        lenguaje.WriteLine("\t\tpublic Lenguaje() : base()");
+                        lenguaje.WriteLine("\t\t{");
+                        lenguaje.WriteLine();
+                        lenguaje.WriteLine("\t\t}");
+                        lenguaje.WriteLine("\t\tpublic Lenguaje(string filePath) : base(filePath)");
+                        lenguaje.WriteLine("\t\t{");
+                        lenguaje.WriteLine();
+                        lenguaje.WriteLine("\t\t}");
+
+                        Producciones();
+                    }
+                    lenguaje.WriteLine("\t}");
+                }
+                lenguaje.WriteLine("}");
             }
             Match("}");
         }
 
         private void Producciones()
         {
-            lenguaje.WriteLine("public void " + getContenido() + "()");
+            lenguaje.WriteLine("\t\tpublic void " + getContenido() + "()");
             Match(c.SNT);
             Match(c.Flechita);
 
-            lenguaje.WriteLine("{");
+            lenguaje.WriteLine("\t\t{");
             LadoDerecho();
-            lenguaje.WriteLine("}");
+            lenguaje.WriteLine("\t\t}");
             lenguaje.WriteLine();
 
             Match(c.FinProduccion);
@@ -48,7 +74,29 @@ namespace Generador
 
         private void LadoDerecho()
         {
+            if (getClasificacion() == c.ST)
+            {
+                lenguaje.WriteLine("\t\t\tMatch(\"" + getContenido() + "\");");
+                Match(c.ST);
 
+            }
+            else if (getClasificacion() == c.SNT)
+            {
+                lenguaje.WriteLine("\t\t\t" + getContenido() + "();");
+                Match(c.SNT);
+            }
+            else if (getClasificacion() == c.ParentesisIzq)
+            {
+                lenguaje.WriteLine("\t\t\t{");
+                Match(c.ParentesisIzq);
+                LadoDerecho();
+                Match(c.ParentesisDer);
+                lenguaje.WriteLine("\t\t\t}");
+            }
+
+            if (getClasificacion() != c.FinProduccion &&
+                getClasificacion() != c.ParentesisDer)
+                LadoDerecho();
         }
     }
 }
--- Generador/Generador/Lexico.cs
+++ Generador/Generador/Lexico.cs
@@ -22,7 +22,7 @@ namespace Generador
         // https://pastebin.com/fCVEbKcx
         private int[,] Trnd = {
             // WS  L   \   ;   -   >   (   )   ?   |   ᵝ
-            {  0,  1,  2,  4,  5,  6,  7,  8,  9, 10, 11  },
+            {  0,  1,  2,  4,  5, 11,  7,  8,  9, 10, 11  },
             {  f,  1,  f,  f,  f,  f,  f,  f,  f,  f,  f  },
             {  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3  },
             {  f,  f,  f,  f,  f,  f,  f,  f,  f,  f,  f  },
